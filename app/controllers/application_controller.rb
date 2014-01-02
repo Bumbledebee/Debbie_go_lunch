0e67d2cb48efcea3ca1ec5554907c346a9c93f13
Debora Blass <debbieblass@gmail.com>
2014-01-02 14:21:45 -0500

    added flash[:notice] to application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:name, :department_id, :lunchgroupleader, :optional]
+    devise_parameter_sanitizer.for(:sign_up) << :name << :department_id <<:lunchgroupleader <<:optional
   end
 end
