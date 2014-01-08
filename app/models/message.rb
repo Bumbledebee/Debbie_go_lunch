f3c6380908fd6e5a99a6afcb2eddcca6b391eabd
Debora Blass <debbieblass@gmail.com>
2014-01-08 15:55:33 -0500

    email regex

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
-  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z+]\z/i
+  VALID_EMAIL = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
   validates_presence_of :body
   validates_format_of :email, with: VALID_EMAIL
