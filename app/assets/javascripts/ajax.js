8a55a880acc11498670ffaeccd16b3d1b270a2f1
Debora Blass <debbieblass@gmail.com>
2014-01-08 21:23:50 -0500

    toggle

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -2,8 +2,22 @@ var removeNotice = function(){
   $('#noticemessage').remove();
 };
 
+var clickOnYes = function() {
+  $(".participating").click(function() {
+    $(".textparticipating").html('I AM COMING');
+  });
+};
 
+var clickOnNO = function() {
+  $(".notParticipating").click(function() {
+    $(".textparticipating").html('NOT ATTENDING');
+  });
+};
 
 $( document ).ready(function() {
   $('#noticemessage').fadeOut(2000, removeNotice);
+  clickOnYes();
+  clickOnNo();
 });
+
+
