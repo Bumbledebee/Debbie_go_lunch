de860ee47c72bb41df9d954a1cb99021d17be899
Debora Blass <debbieblass@gmail.com>
2014-01-20 09:14:15 -0500

    fade out flash messages

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -1,25 +1,10 @@
 var removeNotice = function(){
-  $('#noticemessage').remove();
+  $('.noticemessage').remove();
 };
 
-// var clickOnYes = function() {
-//   $(".participating").click(function() {
-//     $(".textparticipating").html('I AM COMING');
-//   });
-// };
-
-// var clickOnNO = function() {
-//   $(".notParticipating").click(function() {
-//     $(".textparticipating").html('NOT ATTENDING');
-//   });
-// };
 
 $( document ).ready(function() {
-  $('#noticemessage').fadeOut(2000, removeNotice);
-  // clickOnYes();
-  // clickOnNo();
-  // $('.participating').click(function(){
-  // $('.textParticipating').toggle();
+  $('.noticemessage').fadeOut(2000, removeNotice);
 });
 
 
