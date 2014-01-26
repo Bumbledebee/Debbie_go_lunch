fb43235cf331028e5e242a3d63827bad88077feb
Debora Blass <debbieblass@gmail.com>
2014-01-25 20:01:06 -0500

    green test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
 
     if current_user.admin?
-      lunches_path
+      edit_user_path(resource)
     else
       edit_user_path(resource)
     end
